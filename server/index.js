cece43a2dbb5b0401f3c97b18b15598243caee44
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-14 19:35:49 -0800

    (feat) add basic routes for request to entry and search endpoints

--- server/index.js
+++ server/index.js
@@ -4,14 +4,34 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.static('../client/'));
+app.use(express.static(`${__dirname}/../client/`));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (request, response) => {
-  response.send('hello server is running');
+app.get('/timeline/:timelineId', (request, response) => {
+  response.send(request.params);
 });
+
+app.post('/entry', (request, response) => {
+  // for adding an extry to a day model
+  response.send('for adding an extry to a day model');
+});
+
+app.post('/entry/delete', (request, response) => {
+  // for removing an entry from the day model
+  response.send('for removing an entry from the day model');
+})
+
+app.put('/entry', (request, response) => {
+  // for editing a day entry in day model
+  response.send('for editing a day entry in day model');
+});
+
+app.get('/search', (request, response) => {
+  // for triggering a search to the search api
+  response.send('for triggering a search to the search api');
+})
 
 
 const port = 1128;
