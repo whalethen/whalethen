915377d5b2bcb289120432544d346e5b3cb48186
jshen18 <jennshen18@gmail.com>
2017-11-15 11:39:02 -0800

    adding react boilerplate

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,38 @@
-console.log('hello we have some sort of react thing')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      someData: someContainer
+    }
+    this.onSearch = this.onSearch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    
+  }
+}
+
+handleChange(e) {
+  this.setState({
+    term: e.target.value
+  })
+}
+
+
+onSearch(this.state.term) {
+  //ajax get
+  //this.setState.someData: ajax success data
+}
+
+
+render() {
+  return (
+    <div>
+      <h1>SOME TITLE </h1>
+      <Search handleChange={this.handleChange} onSearch={this.onSearch}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
