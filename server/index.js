1a28fbee5a903c1ea06ce79cdcfa432d3d269146
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-15 15:37:58 -0800

    (bug) forgot semicolon

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const api = require('placesApi.js')
+const api = require('placesApi.js');
 
 const app = express();
 
