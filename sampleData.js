b4b439d0a26bef165dbf64f5bd385621426a0f09
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-15 15:32:37 -0800

    removed ds_store and add sample data

---
+++ sampleData.js
@@ -0,0 +1,74 @@
+module.exports.sampleLocation = {
+    "results": [
+        {
+            "address_components": [
+                {
+                    "long_name": "Mountain View",
+                    "short_name": "Mountain View",
+                    "types": [
+                        "locality",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "Santa Clara County",
+                    "short_name": "Santa Clara County",
+                    "types": [
+                        "administrative_area_level_2",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "California",
+                    "short_name": "CA",
+                    "types": [
+                        "administrative_area_level_1",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": [
+                        "country",
+                        "political"
+                    ]
+                }
+            ],
+            "formatted_address": "Mountain View, CA, USA",
+            "geometry": {
+                "bounds": {
+                    "northeast": {
+                        "lat": 37.469887,
+                        "lng": -122.0446721
+                    },
+                    "southwest": {
+                        "lat": 37.3567599,
+                        "lng": -122.1178619
+                    }
+                },
+                "location": {
+                    "lat": 37.3860517,
+                    "lng": -122.0838511
+                },
+                "location_type": "APPROXIMATE",
+                "viewport": {
+                    "northeast": {
+                        "lat": 37.469887,
+                        "lng": -122.0446721
+                    },
+                    "southwest": {
+                        "lat": 37.3567599,
+                        "lng": -122.1178619
+                    }
+                }
+            },
+            "place_id": "ChIJiQHsW0m3j4ARm69rRkrUF3w",
+            "types": [
+                "locality",
+                "political"
+            ]
+        }
+    ],
+    "status": "OK"
+}
