ce301e81a7a69b36860c3dc26a8ef6135b29437c
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-16 10:38:12 -0800

    merge conflicts

--- database/index.js
+++ database/index.js
@@ -1,22 +1,18 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-mongoose.Promise = require('bluebird');
-const options = { promiseLibrary: require('bluebird'), useMongoClient: true };
-
-
-=======
 const Promise = require('bluebird');
 mongoose.Promise = Promise;
 const options = { promiseLibrary: Promise, useMongoClient: true };
 
 mongoose.connect('mongodb://localhost/whaleThen', options);
->>>>>>> (bug) add bluebird to db
 const db = mongoose.connection;
-mongoose.connect('mongodb://localhost/whaleThen', options);
 
-db.on('error', () => console.log('mongoose connection error'));
+db.on('error', () => {
+  console.log('mongoose connection error');
+});
 
-db.once('open', () => console.log('mongoose connected successfully'));
+db.once('open', () => {
+  console.log('mongoose connected successfully');
+});
 
 const eventSchema = mongoose.Schema({
   name: String,
