49b5e88ebecdc7f25060b79d92daa1e171aaa14d
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-19 12:45:33 -0800

    (bug) fix spacing with button

--- client/src/searchBar.jsx
+++ client/src/searchBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const SearchBar = (props) => {
-  return (
-    <div className="searchBar">
-      <input />
-      <button className="searchSubmit" onClick={props.onSubmit}>Search</button>
-    </div>
-  );
-};
+const SearchBar = props => (
+  <div className="searchBar">
+    <input />
+    <button className="searchSubmit" onClick={props.onSubmit}>Search</button>
+  </div>
+);
+
 
 {/*SearchBar.propTypes = {
   props: React.PropTypes.object.isRequired
