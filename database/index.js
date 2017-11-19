f797a66c686d9b365ccf55e9dedd56c26a0b32c8
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-19 15:58:48 -0800

    add saveNewTimeline func

--- database/index.js
+++ database/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
-mongoose.Promise = Promise;
-const options = { promiseLibrary: Promise, useMongoClient: true };
 
-mongoose.connect('mongodb://localhost/whaleThen', options);
+Promise.promisifyAll(mongoose);
+Promise.promisifyAll(require('mongodb'));
+
+mongoose.connect('mongodb://localhost/whaleThen', { useMongoClient: true });
 const db = mongoose.connection;
 
 db.on('error', () => {
@@ -22,9 +23,23 @@ const eventSchema = mongoose.Schema({
 
 const daySchema = mongoose.Schema({
   day: Number,
-  id: Number,
+  timelineId: Number,
   events: [eventSchema],
 });
 
 const Day = mongoose.model('Day', daySchema);
 const Event = mongoose.model('Event', eventSchema);
+
+const saveNewTimeline = (timelineId, numberOfDays) => {
+  const timeline = [];
+  for (let day = 1; day <= numberOfDays; day += 1) {
+    const newDay = new Day({ day, timelineId });
+    timeline.push(newDay);
+  }
+
+  return Promise.map(timeline, day => day.save());
+}
+
+saveNewTimeline(1234, 5)
+.then(result => console.log(result))
+.catch(err => console.error('error: ', err))
