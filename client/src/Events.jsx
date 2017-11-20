4dfddb2fede5944795ac81c0ac477ce6d930334d
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-20 14:52:15 -0800

    fix arrow funcs to be inline with styleguide

--- client/src/Events.jsx
+++ client/src/Events.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 
-const Events = (props) => {
-  return (
-    <div className="event">
-      <div className="eventName">Event Name</div>
-      <div className="description">description</div>
-      <div className="vote">votes</div>
-    </div>
-  );
-};
+const Events = props => (
+  <div className="event">
+    <div className="eventName">Event Name</div>
+    <div className="description">description</div>
+    <div className="vote">votes</div>
+  </div>
+);
+
 
 // Events.propTypes = {
 //   SOMEDATA: React.PropTypes.object.isRequired
--- client/src/StartDateBox.jsx
+++ client/src/StartDateBox.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const StartDateBox = ({ onInput, onEnter }) => {
-  return (
-    <div className="inputBox">
-      <label className="startDate" htmlFor="startDate">
-      Start Date:
-      <input
-        id="startDate"
-        type="text"
-        name="startDate"
-        onChange={event => onInput(event)}
-        placeholder="enter a start date"
-        onKeyUp={event => onEnter(event)}
-      />
-      </label>
-    </div>
-  );
-};
+const StartDateBox = ({ onInput, onEnter }) => (
+  <div className="inputBox">
+    <label className="startDate" htmlFor="startDate">
+    Start Date:
+    <input
+      id="startDate"
+      type="text"
+      name="startDate"
+      onChange={event => onInput(event)}
+      placeholder="enter a start date"
+      onKeyUp={event => onEnter(event)}
+    />
+    </label>
+  </div>
+);
+
 
 export default StartDateBox;
