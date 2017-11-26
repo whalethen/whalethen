78436c80404ba7872fac2550a11b2d23dc021829
= <=>
2017-11-25 19:02:02 -0800

    added some buttons for upvote & downvote

--- client/src/Events.jsx
+++ client/src/Events.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 
-const Events = ({ event }) => (
-  <div className="event">
-    <div className="eventName">{event.name}</div>
-    <div className="description">{event.address}</div>
-    <div className="vote">Votes: {event.votes}</div>
-  </div>
-);
+class Events extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			votes: this.props.event.votes,
+		}
+		this.updateVotes = this.updateVotes.bind(this);
+	}
+
+	updateVotes(e) {
+		console.log('hey this event is what Brian was looking for', this.props);
+		if (e.target.value === "+") {
+			this.setState({
+				votes: this.state.votes += 1,
+			});
+		} else {
+			this.setState({
+				votes: this.state.votes -= 1,
+			});
+		}
+
+	}
+
+	render() {
+	  return (<div className="event">
+	    <div className="eventName">{this.props.event.name}</div>
+	    <div className="description">{this.props.event.address}</div>
+	    <span className="vote">{` Votes: ${this.state.votes}   `}  
+	    	<button className="votes" value="+" onClick={this.updateVotes}>+</button>
+	    	<button className="votes" value="-" onClick={this.updateVotes}>-</button>
+	    </span>
+	  </div>
+	  )
+	}
+};
 
 
 Events.propTypes = {
