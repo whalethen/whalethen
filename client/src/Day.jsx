7c07b046c41c9f0975f47ec86bde80773d278c0a
= <=>
2017-11-26 19:15:24 -0800

    edit props to pass in day # and timelineId

--- client/src/Day.jsx
+++ client/src/Day.jsx
@@ -3,11 +3,11 @@ import _ from 'lodash';
 import propTypes from 'prop-types';
 import Event from './Events';
 
-const Day = ({ day }) => (
+const Day = ({ day, timelineId }) => (
   <div className="dayView">
     <div className="dayTitle">Day {day.day}</div>
     <div className="events">
-      {_.map(day.events, (event, index) => <Event event={event} key={index} />)}
+      {_.map(day.events, (event, index) => <Event day={day} timelineId={timelineId} event={event} key={index} />)}
     </div>
   </div>
 );
