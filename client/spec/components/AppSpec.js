c751ad655e0e3ca8615c8e84609b73f7b7e4c402
chalkera <achalker@gmail.com>
2017-11-28 23:45:02 -0800

    Created a babelrc file that will compile all jsx code into js to run any test and all it will need is npm run compile to do it for you

---
+++ client/spec/components/AppSpec.js
@@ -0,0 +1,24 @@
+describe('App', () => {
+  // const {
+  //   Simulate,
+  //   renderIntoDocument,
+  //   findRenderedDOMComponentWithClass,
+  //   scryRenderedDOMComponentsWithClass,
+  // } = React.addons.TestUtils;
+  //
+  // let app;
+  //
+  // beforeEach(() => {
+  //   const app = renderIntoDocument(<App />);
+  // });
+  //
+  // it('App should be a stateful class component', () => {
+  //   expect(React.Component.isPrototypeOf(App)).to.be.false;
+  // });
+  xit('Should show a passing test', () => {
+    expect(true).to.be.true;
+  });
+  xit('Should show a failing test', () => {
+    expect(true).to.be.false;
+  });
+});
--- client/src/searchBar.jsx
+++ client/src/searchBar.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
- 
+import propTypes from 'prop-types';
 
-const SearchBar = (props) => {
-  return (
-    <div>
-      <button onClick={props.onSubmit}>Search Things To Do</button>
-    </div>
-  );
+const SearchBar = props => (
+  <div className="searchBar">
+    <input
+      id="location"
+      type="text"
+      placeholder="location"
+      onChange={props.handleLoc}
+      onKeyUp={event => props.onEnter(event)}
+    />
+    <input
+      id="category"
+      type="text"
+      placeholder="category"
+      onChange={props.handleCat}
+      onKeyUp={event => props.onEnter(event)}
+    />
+    <button className="searchSubmit" onClick={props.onSubmit}>Search</button>
+  </div>
+);
+
+SearchBar.propTypes = {
+  onSubmit: propTypes.func.isRequired,
+  onEnter: propTypes.func.isRequired,
+  handleLoc: propTypes.func.isRequired,
+  handleCat: propTypes.func.isRequired,
 };
 
-{/*SearchBar.propTypes = {
-  props: React.PropTypes.object.isRequired
-};*/}
 
 export default SearchBar;
