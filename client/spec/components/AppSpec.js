def372bbbe97a0f996f66f5a4e13c968c307e39b
chalkera <achalker@gmail.com>
2017-11-29 08:29:31 -0800

    Code cleanup

--- client/spec/components/AppSpec.js
+++ client/spec/components/AppSpec.js
@@ -1,24 +1,10 @@
+/* eslint-disable */
 describe('App', () => {
-  // const {
-  //   Simulate,
-  //   renderIntoDocument,
-  //   findRenderedDOMComponentWithClass,
-  //   scryRenderedDOMComponentsWithClass,
-  // } = React.addons.TestUtils;
-  //
-  // let app;
-  //
-  // beforeEach(() => {
-  //   const app = renderIntoDocument(<App />);
-  // });
-  //
-  // it('App should be a stateful class component', () => {
-  //   expect(React.Component.isPrototypeOf(App)).to.be.false;
-  // });
-  xit('Should show a passing test', () => {
+
+  it('Should show a passing test', () => {
     expect(true).to.be.true;
   });
-  xit('Should show a failing test', () => {
+  it('Should show a failing test', () => {
     expect(true).to.be.false;
   });
 });
